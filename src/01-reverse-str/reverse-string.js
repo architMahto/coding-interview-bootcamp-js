5c6a4140733a94c5018460bbe9491e8cdcf79bad
Archit Mahto <architmahto@gmail.com>
2018-11-02 05:25:19 -0400

    Implemented 3 ways to reverse a string

--- src/01-reverse-str/reverse-string.js
+++ src/01-reverse-str/reverse-string.js
@@ -5,25 +5,33 @@
  * @returns {string} The reverse of str
  */
 export function reverseStr(str) {
-	return str;
+	return str.split('').reverse().join('');
 }
 
 /**
- * Reverses a string
+ * Reverses a string using a for loop
  *
  * @param str
  * @returns {string} The reverse of str
  */
 export function reverseStr2(str) {
-	return str;
+	let newStr = '';
+
+	for (let i = str.length - 1; i >= 0; i--) {
+		newStr += str[i];
+	}
+
+	return newStr;
 }
 
 /**
- * Reverses a string
+ * Reverses a string using the reducer helper
  *
  * @param str
  * @returns {string} The reverse of str
  */
 export function reverseStr3(str) {
-	return str;
+	return str.split('').reduce((reversed, char) => {
+		return char + reversed;
+	}, '');
 }
--- src/01-reverse-str/reverse-string.test.js
+++ src/01-reverse-str/reverse-string.test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { reverseStr, reverseStr2, reverseStr3 } from './reverse-string';
 
-describe('Testing the reverseStr function', function () {
+describe.only('Testing the reverseStr function', function () {
 	it('should exist', function () {
 		expect(reverseStr).to.not.be.undefined;
 	});
@@ -11,19 +11,19 @@ describe('Testing the reverseStr function', function () {
 	});
 });
 
-describe('Testing the reverseStr2 function', function () {
+describe.only('Testing the reverseStr2 function', function () {
 	it('should exist', function () {
 		expect(reverseStr2).to.not.be.undefined;
 	});
 
 	it('should return dcba for abcd', () => {
-		expect(reverseStr3('abcd')).to.equal('dcba');
+		expect(reverseStr2('abcd')).to.equal('dcba');
 	});
 });
 
-describe('Testing the reverseStr3 function', function () {
+describe.only('Testing the reverseStr3 function', function () {
 	it('should exist', function () {
-		expect(reverseStr2).to.not.be.undefined;
+		expect(reverseStr3).to.not.be.undefined;
 	});
 
 	it('should return dcba for abcd', () => {
