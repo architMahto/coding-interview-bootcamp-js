0a81e2605758e5e722826316269cfbc70a8afc0e
Archit Mahto <architmahto@gmail.com>
2018-11-04 17:10:38 -0500

    Implemented recursive solution for printing a pyramid

--- src/09-pyramids/pyramids.js
+++ src/09-pyramids/pyramids.js
@@ -1,5 +1,5 @@
 /**
- * Prints out a pyramid shape with 'num' levels using the # character
+ * Prints out a pyramid shape with 'num' levels using the # character iteratively
  *
  * @param num
  */
@@ -21,4 +21,34 @@ export function iterativePyramids(num) {
 
 		console.log(level);
 	}
+}
+
+/**
+ * Prints out a pyramid shape with 'num' levels using the # character recursively
+ *
+ * @param num
+ * @param row
+ * @param col
+ * @param level
+ */
+export function recursivePyramids(num, row = 0, col = 0, level = '') {
+	let cols = (2*num) - 1;
+	let midpoint = Math.floor(cols/2);
+
+	if (row === num) {
+		return;
+	}
+
+	if (col === cols) {
+		console.log(level);
+		return recursivePyramids(num, row + 1);
+	}
+
+	if (((midpoint - row) <= col) && ((midpoint + row) >= col)) {
+		level += '#';
+	} else {
+		level += ' ';
+	}
+
+	recursivePyramids(num, row, col + 1, level);
 }
--- src/09-pyramids/pyramids.test.js
+++ src/09-pyramids/pyramids.test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { spy } from 'sinon';
-import { iterativePyramids } from './pyramids';
+import { iterativePyramids, recursivePyramids } from './pyramids';
 
 describe.only('Testing the iterativePyramids method', function () {
 	beforeEach(function () {
@@ -30,6 +30,47 @@ describe.only('Testing the iterativePyramids method', function () {
 
 	it('should call console.log 4 times num = 4', function () {
 		iterativePyramids(4);
+
+		expect(console.log.getCalls().length).to.equal(4);
+		expect(console.log.getCall(0).args[0]).to.equal('   #   ');
+		expect(console.log.getCall(1).args[0]).to.equal('  ###  ');
+		expect(console.log.getCall(2).args[0]).to.equal(' ##### ');
+		expect(console.log.getCall(3).args[0]).to.equal('#######');
+	});
+
+	afterEach(function () {
+		console.log.restore();
+	});
+});
+
+describe.only('Testing the recursivePyramids method', function () {
+	beforeEach(function () {
+		spy(console, 'log');
+	});
+
+	it('should exist', function () {
+		expect(recursivePyramids).to.not.be.undefined;
+	});
+
+	it('should call console.log 2 times num = 2', function () {
+		recursivePyramids(2);
+
+		expect(console.log.getCalls().length).to.equal(2);
+		expect(console.log.getCall(0).args[0]).to.equal(' # ');
+		expect(console.log.getCall(1).args[0]).to.equal('###');
+	});
+
+	it('should call console.log 3 times num = 3', function () {
+		recursivePyramids(3);
+
+		expect(console.log.getCalls().length).to.equal(3);
+		expect(console.log.getCall(0).args[0]).to.equal('  #  ');
+		expect(console.log.getCall(1).args[0]).to.equal(' ### ');
+		expect(console.log.getCall(2).args[0]).to.equal('#####');
+	});
+
+	it('should call console.log 4 times num = 4', function () {
+		recursivePyramids(4);
 
 		expect(console.log.getCalls().length).to.equal(4);
 		expect(console.log.getCall(0).args[0]).to.equal('   #   ');
