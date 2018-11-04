0b0788bc4310416af7c7dac53c664c1fa60fba92
Archit Mahto <architmahto@gmail.com>
2018-11-03 22:36:10 -0400

    Implemented solution for chunking array

--- src/05-chunking/chunk-array.js
+++ src/05-chunking/chunk-array.js
@@ -6,5 +6,15 @@
  * @returns {array} An array of subarrays
  */
 export function chunkArray(array, size) {
-	return array;
+	// if (!array.isArray()) {
+	// 	return [];
+	// }
+
+	const firstChunk = array.slice(0, size); // create the first chunk of the given array
+
+	if (!firstChunk.length) {
+		return array; // this is the base case to terminal the recursive
+	}
+
+	return [firstChunk].concat(chunkArray(array.slice(size, array.length), size));
 }
