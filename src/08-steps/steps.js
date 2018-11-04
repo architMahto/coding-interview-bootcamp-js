b8ad5199e28fda10b973b79ccfb5636c245ad899
Archit Mahto <architmahto@gmail.com>
2018-11-04 12:36:21 -0500

    Implemented iterative solution for printing stairs

--- src/08-steps/steps.js
+++ src/08-steps/steps.js
@@ -1,8 +1,30 @@
 /**
- * Prints out a step shape with 'num' levels using the # character
+ * Prints out a step shape with 'num' levels using the # character iteratively
  *
  * @param num
  */
-export function steps(num) {
+export function iterativeSteps(num) {
+	for (let row = 0; row < num; row++) {
+		let step = '';
+
+		for (let col = 0; col < num; col++) {
+			if (col <= row) {
+				step += '#';
+			} else {
+				step += ' ';
+			}
+		}
+
+		console.log(step);
+	}
+
+}
+
+/**
+ * Prints out a step shape with 'num' levels using the # character recursively
+ *
+ * @param num
+ */
+export function recursiveSteps(num) {
 	console.log('');
 }
--- src/08-steps/steps.test.js
+++ src/08-steps/steps.test.js
@@ -1,25 +1,25 @@
 import { expect } from 'chai';
 import { spy } from 'sinon';
-import { steps } from './steps';
+import { iterativeSteps, recursiveSteps } from './steps';
 
-describe('Testing the steps method', function () {
+describe.only('Testing the iterativeSteps method', function () {
 	beforeEach(function () {
 		spy(console, 'log');
 	});
 
 	it('should exist', function () {
-		expect(steps).to.not.be.undefined;
+		expect(iterativeSteps).to.not.be.undefined;
 	});
 
 	it('should call console.log 1 time for num = 1', function () {
-		steps(1);
+		iterativeSteps(1);
 
 		expect(console.log.getCalls().length).to.equal(1);
 		expect(console.log.getCall(0).args[0]).to.equal('#');
 	});
 
 	it('should call console.log 2 times for num = 2', function () {
-		steps(2);
+		iterativeSteps(2);
 
 		expect(console.log.getCalls().length).to.equal(2);
 		expect(console.log.getCall(0).args[0]).to.equal('# ');
@@ -27,11 +27,49 @@ describe('Testing the steps method', function () {
 	});
 
 	it('should call console.log 3 times for num = 3', function () {
-		steps(3);
+		iterativeSteps(3);
 
 		expect(console.log.getCalls().length).to.equal(3);
+		expect(console.log.getCall(0).args[0]).to.equal('#  ');
+		expect(console.log.getCall(1).args[0]).to.equal('## ');
+		expect(console.log.getCall(2).args[0]).to.equal('###');
+	});
+
+	afterEach(function () {
+		console.log.restore();
+	});
+});
+
+describe.only('Testing the recursiveSteps method', function () {
+	beforeEach(function () {
+		spy(console, 'log');
+	});
+
+	it('should exist', function () {
+		expect(recursiveSteps).to.not.be.undefined;
+	});
+
+	it('should call console.log 1 time for num = 1', function () {
+		recursiveSteps(1);
+
+		expect(console.log.getCalls().length).to.equal(1);
+		expect(console.log.getCall(0).args[0]).to.equal('#');
+	});
+
+	it('should call console.log 2 times for num = 2', function () {
+		recursiveSteps(2);
+
+		expect(console.log.getCalls().length).to.equal(2);
 		expect(console.log.getCall(0).args[0]).to.equal('# ');
 		expect(console.log.getCall(1).args[0]).to.equal('##');
+	});
+
+	it('should call console.log 3 times for num = 3', function () {
+		recursiveSteps(3);
+
+		expect(console.log.getCalls().length).to.equal(3);
+		expect(console.log.getCall(0).args[0]).to.equal('#  ');
+		expect(console.log.getCall(1).args[0]).to.equal('## ');
 		expect(console.log.getCall(2).args[0]).to.equal('###');
 	});
 
