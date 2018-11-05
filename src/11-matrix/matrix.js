35bd63dcc207e7d5c028408745bf82931f21eae2
Archit Mahto <architmahto@gmail.com>
2018-11-04 20:39:17 -0500

    Implemented solution for creating a spiral matrix

--- src/11-matrix/matrix.js
+++ src/11-matrix/matrix.js
@@ -5,5 +5,48 @@
  * @returns {Array} A num X num spiral matrix
  */
 export function matrix(num) {
-	return [];
+	const result = [];
+	let counter = 1;
+
+	for (let i = 0; i < num; i++) {
+		result.push([]);
+	}
+
+	let [startCol, startRow, endCol, endRow] = [0, 0, num - 1, num - 1];
+
+	while (startCol <= endCol && startRow <= endRow) {
+		// Assign values at starting row
+		for (let col = startCol; col <= endCol; col++) {
+			result[startRow][col] = counter;
+			counter++;
+		}
+
+		startRow++;
+
+		// Assign values at end column
+		for (let row = startRow; row <= endRow; row++) {
+			result[row][endCol] = counter;
+			counter++;
+		}
+
+		endCol--;
+
+		// Assign values at end row
+		for (let col = endCol; col >= startCol; col--) {
+			result[endRow][col] = counter;
+			counter++;
+		}
+
+		endRow--;
+
+		// Assign values at starting column
+		for (let row = endRow; row >= startRow; row--) {
+			result[row][startCol] = counter;
+			counter++;
+		}
+
+		startCol++;
+	}
+
+	return result;
 }
--- src/11-matrix/matrix.test.js
+++ src/11-matrix/matrix.test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { matrix } from './matrix';
 
-describe('Testing the matrix function', function () {
+describe.only('Testing the matrix function', function () {
 	it('should exist', function () {
 		expect(matrix).to.not.be.undefined;
 	});
@@ -9,27 +9,27 @@ describe('Testing the matrix function', function () {
 	it('should produce a 2x2 array for matrix(2)', function () {
 		const m = matrix(2);
 
-		expect(m.length).to.equal(2);
-		expect(m[0]).to.equal([1, 2]);
-		expect(m[1]).to.equal([3, 4]);
+		expect(m.length).to.deep.equal(2);
+		expect(m[0]).to.deep.equal([1, 2]);
+		expect(m[1]).to.deep.equal([4, 3]);
 	});
 
 	it('should produce a 3x3 array for matrix(3)', function () {
 		const m = matrix(3);
 
-		expect(m.length).to.equal(3);
-		expect(m[0]).to.equal([1, 2, 3]);
-		expect(m[1]).to.equal([8, 9, 4]);
-		expect(m[2]).to.equal([7, 6, 5]);
+		expect(m.length).to.deep.equal(3);
+		expect(m[0]).to.deep.equal([1, 2, 3]);
+		expect(m[1]).to.deep.equal([8, 9, 4]);
+		expect(m[2]).to.deep.equal([7, 6, 5]);
 	});
 
 	it('should produce a 4x4 array for matrix(4)', function () {
 		const m = matrix(4);
 
 		expect(m.length).to.equal(4);
-		expect(m[0]).to.equal([1, 2, 3, 4]);
-		expect(m[1]).to.equal([12, 13, 14, 5]);
-		expect(m[2]).to.equal([11, 16, 15, 6]);
-		expect(m[3]).to.equal([10, 9, 8, 7]);
+		expect(m[0]).to.deep.equal([1, 2, 3, 4]);
+		expect(m[1]).to.deep.equal([12, 13, 14, 5]);
+		expect(m[2]).to.deep.equal([11, 16, 15, 6]);
+		expect(m[3]).to.deep.equal([10, 9, 8, 7]);
 	});
 });
